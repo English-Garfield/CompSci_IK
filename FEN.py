d9e52949e91ad3a779a5402d32035d95a418b62d
English-Garfield <104942741+English-Garfield@users.noreply.github.com>
2024-04-24 09:10:51 +0100

    changes to FEN around checking white and black pieces
    main has start of game with board on a screen

--- FEN.py
+++ FEN.py
@@ -1,8 +1,23 @@
-def FEN():
+def translater():
     fenArray = []
     fenString = input("What is the FEN code: ")
 
     for char in fenString:
         fenArray.append(char)
 
+    for i in (len(fenArray) + 1):
+        piece = fenArray[0]
 
+        if piece.isupper():
+            # piece is white
+            print("piece is white")
+
+        elif piece.islower():
+            # piece is black
+            print("piece is black")
+
+        else:
+            # number
+            print("piece is black")
+
+        del (fenArray[0])
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 # Imports
+import sys
 import pygame
 import FEN
 from sys import maxsize
 
 pygame.init()
 
+# Set up window
+size = (640, 640)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Chess game")
 
+# Set up board
+board = pygame.Surface((600, 600))
+board.fill((255, 206, 158))
 
+# Draw the Board
+for x in range(0, 8, 2):
+    for y in range(0, 8, 2):
+        pygame.draw.rect(board, (210, 180, 140), (x * 75, y * 75, 75, 75))
+        pygame.draw.rect(board, (210, 180, 140), ((x + 1) * 75, (y + 1) * 75, 75, 75))
 
+# adding board to screen
+screen.blit(board, (20, 20))
 
+pygame.display.flip()
 
-
-
+# main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
