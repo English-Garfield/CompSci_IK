046d02e63243d534b07c130227cbe517c64f58fd
English-Garfield <104942741+English-Garfield@users.noreply.github.com>
2024-04-24 10:58:00 +0100

    all pieces but king and queen

--- main.py
+++ main.py
@@ -48,6 +48,28 @@ for i in range(8):
     pieces.append(Piece("Black", i, 1, "Pawn"))
     pieces.append(Piece("White", i, 6, "Pawn"))
 
+# Rooks
+pieces.append(Piece("Black", 0, 0, "Rook"))
+pieces.append(Piece("Black", 7, 0, "Rook"))
+
+pieces.append(Piece("White", 0, 7, "Rook"))
+pieces.append(Piece("White", 7, 7, "Rook"))
+
+# Knights
+pieces.append(Piece("Black", 1, 0, "Knight"))
+pieces.append(Piece("Black", 6, 0, "Knight"))
+
+pieces.append(Piece("White", 1, 7, "Knight"))
+pieces.append(Piece("White",6, 7, "Knight"))
+
+# Bishops
+pieces.append(Piece("Black", 2, 0, "Bishop"))
+pieces.append(Piece("Black", 5, 0, "Bishop"))
+
+pieces.append(Piece("White", 2, 7, "Bishop"))
+pieces.append(Piece("White",5, 7, "Bishop"))
+
+
 # Draw the pieces
 for piece in pieces:
     piece.draw(board)
@@ -58,3 +80,36 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # get the position of the click
+            pos = pygame.mouse.get_pos()
+
+            # convert the position to board coordinates
+            x = (pos[0] - 20) // 75
+            y = (pos[1] - 20) // 75
+
+            # find the piece at the clicked position
+            for piece in pieces:
+                if piece.x == x and piece.y == y:
+                    # move the piece
+                    pos = pygame.mouse.get_pos()
+                    x = (pos[0] - 20) // 75
+                    y = (pos[1] - 20) // 75
+                    piece.x = x
+                    piece.y = y
+
+    # redraw the board and pieces
+    board.fill((255, 206, 158))
+    for x in range(0, 8, 2):
+        for y in range(0, 8, 2):
+            pygame.draw.rect(board, (210, 180, 140), (x * 75, y * 75, 75, 75))
+            pygame.draw.rect(board, (210, 180, 140), ((x + 1) * 75, (y + 1) * 75, 75, 75))
+
+    for piece in pieces:
+        piece.draw(board)
+
+    # add the board to the screen
+    screen.blit(board, (20, 20))
+
+    # update the display
+    pygame.display.update()
