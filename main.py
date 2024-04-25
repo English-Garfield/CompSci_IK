97a12c975c945f14d4237f0982d2234bc8f2ee84
English-Garfield <104942741+English-Garfield@users.noreply.github.com>
2024-04-25 11:42:53 +0100

    simplified imports

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 #       19/04/2024       #
 
 # Imports
-from ChessItems import Chess_Game as Chess
 from ChessItems import FEN as fen
-from ChessItems.Chess_Game import game
-
+from ChessItems import Chess_Game as Chess
 # Main
 Chess.game()
 
